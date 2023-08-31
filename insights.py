0f0eeaf4bb6dec0b9034d5bc774ea4d0c356e70d
Paul Bogumilo <18363413+bogumilo@users.noreply.github.com>
2023-08-31 23:39:18 +0200

    feat: parse commandline arguments with argparse instead of sys.argv

--- insights.py
+++ insights.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 from websocket import create_connection
 import json
 import time
@@ -9,11 +10,11 @@ import pandas as pd
 
 SOCKET_URI = "wss://ws-feed-public.sandbox.exchange.coinbase.com"
 
+SUPPORTED_PRODUCTS = ['BTC-USD']
+
 subscribe_message = {
         "type": "subscribe",
-        "product_ids": [
-            'BTC-USD'
-        ],
+        "product_ids": SUPPORTED_PRODUCTS,
         "channels": ["level2_batch"]
     }
 
@@ -63,21 +64,33 @@ def process_batch_messages(interval):
         dt = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))
         print(f'Batch at {dt} insights: \n {insights_df} \n')
 
+def validate_product_id(product_id):
+    if product_id.upper() not in SUPPORTED_PRODUCTS:
+        raise argparse.ArgumentTypeError(f"Unsupported product '{product_id.upper()}'. Supported products are {SUPPORTED_PRODUCTS}")
+    return product_id.upper()
+
 def main():
     '''Ask user for a crypto product_id they are interested in'''
-    if len(sys.argv) > 1:
-        query = str(sys.argv[1])
+    parser = argparse.ArgumentParser(description="Generate insights for a provided crypto product.")
+    parser.add_argument("product_id", help="Provide crypto product to generate insights in format e.g. BTC-USD", type=validate_product_id, nargs="?")
+
+    args = parser.parse_args()
+    if args.product_id:
+        product_id = args.product_id
     else:
-        query = input("Provide crypto product to generate insights in format e.g. BTC-USD :\n> ")
-    product_id = query.upper()
-    if not product_id == 'BTC-USD':
-        print(f"Error: Current websocket doesn't support '{query.upper()}' product. Try again")
-    else:
-        # Start the function in a separate thread
+        while True:
+            try:
+                product_id = input("Provide crypto product to generate insights in format e.g. BTC-USD :\n> ")
+                product_id = validate_product_id(product_id)
+                break
+            except argparse.ArgumentTypeError as e:
+                print(str(e))
+
+    try:
         threading.Thread(target=process_batch_messages, args=(5,)).start()
 
         ws = create_connection(SOCKET_URI)
-        print("websocket opened")
+        print("WebSocket opened")
         ws.send(json.dumps(subscribe_message))
 
         while True:
@@ -85,6 +98,15 @@ def main():
             transformed_message = transform_message(message)
             if transformed_message is not None:
                 message_queue.put(transformed_message)
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+    finally:
+        if ws:
+            ws.close()
+        print("\nWebSocket closed")
+
 
 if __name__ == '__main__':
     try:
