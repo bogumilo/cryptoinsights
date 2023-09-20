ea5ee5f96e82274b27c3698a9a032fa44c3586e8
bogumilo <18363413+bogumilo@users.noreply.github.com>
2023-09-20 13:29:14 +0200

    fix: add signal_handler, gargabe collector

--- insights.py
+++ insights.py
@@ -9,6 +9,9 @@ from tabulate import tabulate
 import pandas as pd
 from collections import defaultdict
 from concurrent.futures import ThreadPoolExecutor
+import signal
+import sys
+import gc
 
 SOCKET_URI = "wss://ws-feed-public.sandbox.exchange.coinbase.com"
 
@@ -23,13 +26,12 @@ subscribe_message = {
 # Queue to hold messages
 message_queue = queue.Queue()
 
-# Create a ThreadPoolExecutor
-executor = ThreadPoolExecutor(max_workers=1)
-
 # # Define the interval
-# interval = 5
+interval = 5
 
 D = {'tz':[], 'highest_bid': [], 'lowest_ask': [],'difference': [], 'highest_bid_qty': [], 'lowest_ask_qty': [], 'mid_price': []}
+
+ws = None
 
 def transform_message(message):
     result = json.loads(message)
@@ -144,6 +146,19 @@ def validate_product_id(product_id):
         raise argparse.ArgumentTypeError(f"Unsupported product '{product_id.upper()}'. Supported products are {SUPPORTED_PRODUCTS}")
     return product_id.upper()
 
+def signal_handler(sig, frame):
+    global ws
+    print('You pressed Ctrl+C!')
+    if ws:
+        ws.close()
+    print("\nWebSocket closed")
+    # Clean up unused resources
+    gc.collect()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
 def main():
     '''Ask user for a crypto product_id they are interested in'''
     # Create an argument parser
@@ -169,39 +184,35 @@ def main():
     ws = None
     try:
         # Set the interval for processing batch messages
-        interval = 5
+        # interval = 5
         # Submit the process_batch_messages function to be run in a separate thread
-        executor.submit(process_batch_messages, interval)
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            executor.submit(process_batch_messages, interval)
 
-        # Open a WebSocket connection
-        ws = create_connection(SOCKET_URI)
-        print("WebSocket opened")
-        # Send a subscribe message over the WebSocket
-        ws.send(json.dumps(subscribe_message))
+            # Open a WebSocket connection
+            ws = create_connection(SOCKET_URI)
+            print("WebSocket opened")
+            # Send a subscribe message over the WebSocket
+            ws.send(json.dumps(subscribe_message))
 
-        # Continuously receive messages from the WebSocket, transform them, and add them to the queue
-        while True:
-            message = ws.recv()
-            transformed_message = transform_message(message)
-            if transformed_message is not None:
-                message_queue.put(transformed_message)
+            # Continuously receive messages from the WebSocket, transform them, and add them to the queue
+            while True:
+                # Set an alarm for some number of seconds (e.g., 5 seconds)
+                signal.alarm(30)
+                try:
+                    message = ws.recv()
+                    transformed_message = transform_message(message)
+                    if transformed_message is not None:
+                        message_queue.put(transformed_message)
+                except TimeoutError:
+                    print("Timeout occurred")
+                finally:
+                    # Cancel the alarm
+                    signal.alarm(0)
 
     except Exception as e:
         # If an error occurs, print it
         print(f"An error occurred: {str(e)}")
 
-    finally:
-        # Close the WebSocket connection when done
-        if ws:
-            ws.close()
-        print("\nWebSocket closed")
-
-
 if __name__ == '__main__':
-    try:
-        main()
-        while len(sys.argv) == 1:
-            main()
-    except KeyboardInterrupt:
-        print('\nGoodbye!')
-        sys.exit(0)
+    main()
